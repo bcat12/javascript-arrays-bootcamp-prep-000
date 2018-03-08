d80c3b1c489124f73706d9e7eb260c42ebbe1b3c
Brian Caterfino <bcaterfino@gmail.com>
2018-03-08 18:39:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,4 +7,5 @@ function accessElementInArray()
 function destructivelyRemoveElementFromBeginningOfArray()
 function removeElementFromBeginningOfArray()
 function destructivelyRemoveElementFromEndOfArray()
-function 
+function removeElementFromEndOfArray ()
+
