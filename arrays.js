5229347bb6c320324e189d0dd361becb85794dea
Brian Caterfino <bcaterfino@gmail.com>
2018-03-08 18:55:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element){
- return [element, ...array];
-}
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];	+  return [element, ...array]
+ }	 }
  
 function destructivelyAddElementToBeginningOfArray()
 function addElementToEndOfArray()
